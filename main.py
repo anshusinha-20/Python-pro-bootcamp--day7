7dd174e0db2eb2f4b3e0537d3ab24435f0e71505
Anshu Sinha <anshujuly2@gmail.com>
2023-03-03 21:56:23 +0530

    Modified hangman project files

--- main.py
+++ main.py
@@ -1,65 +1,48 @@
-"""import statements"""
-import words
-import hangman
+"""imports random module"""
 import random
-
-"""this print statement is used to print the logo from the hangman module"""
-print(hangman.logo)
-
-"""this word variable stores the random generated word from the words list from the hangman module"""
-word = random.choice(words.words)
-
-"""this print function gives the generated word"""
-print(f"The answer is: {word}")
-
-"""this variable stores the hangman inital stages from the hangman module"""
-initialStage = hangman.initialStage
-
-"""this variable hold the list of stages from the hangman module"""
-stages = [hangman.stage1, hangman.stage2, hangman.stage3, hangman.stage4, hangman.stage5, hangman.stage6, hangman.stage7]
-
-"""this variable hold the number of lives"""
-lives = 7
-
-"""this variable holds the number of _ generated which is equivalent to the number of characters of the randomly generated word"""
-blankSpaces = ["_" for i in word]
-
-"""this variable holds the stage"""
-stage = initialStage
-
-"""this variable is set to value of false"""
-
-endOfGame = False
-
-"""this loop will run until the game is not over"""
-while (endOfGame == False):
-    guess = input("Guess a letter: ")
-        
-    if guess in word:
-        for i in range(len(word)):
-            if guess == word[i]:
-                blankSpaces[i] = guess
-
-        for i in blankSpaces:
-            print(i, end='')
-            
-        print(stage)
-
-        if "_" not in blankSpaces:
-            print("You won!")
-            endOfGame = True
-
-    else :
-        print(f"You guessed {guess}, that's not in the world. You lose a life!")
-        for i in blankSpaces:
-            print(i, end='')
-        stage = stages[-lives]
-        lives -= 1
-        print(stage)
-
-        if lives == 0:
-            print("You lose!")
-            endOfGame = True
-
-        
-
+"""prints the initial project text"""
+print("Welcome to The Number Guessing Game")
+"""print the initial project text"""
+print("I'm thinking of a number between 1 and 100.")
+"""stores the randomly generated number between 1 and 100, inclusive of 1 and 100"""
+answer = random.randint(1, 100)
+"""prints the hint to the correct answer"""
+print(f"Pssst, the correct answer is {answer}")
+"""asks the user to chose the difficulty level"""
+difficultyLevel = input("Chose a difficulty level. Type 'e' for easy and 'h' for hard: ").lower()
+"""if difficulty level is easy, attempts are set to 10 else attempts are set to 5"""
+if (difficultyLevel == 'e'):
+    attempts = 10
+elif (difficultyLevel == 'h'):
+    attempts = 5
+"""prints the number of attempts a user has"""
+print(f"You have {attempts} attempts remaining to guess the number.")
+"""function definition"""
+def guessTheNumber():
+    """attempts variable is accessed and modified inside this function"""
+    global attempts
+    """asks the user to make a guess"""
+    guess = int(input("Make a guess: "))
+    """if guess is lower than the numerical value of the answer, the 'too low' is printed"""
+    if (guess < answer):
+        print("Too low.")
+        """else if the guess is higher then answer, than 'too high' is printed"""
+    elif (guess > answer):
+        print("Too high.")
+    """unitl attempts are bigger than 1, this loop runs"""
+    while (attempts > 1):
+        """if guess is equal to the answer, the user wins and the loop breaks"""
+        if (guess == answer):
+            print(f"You got it! The answer was {answer}")
+            break
+            """else the user is asked to guess again, attempts are deducted by 1 and the function is called again"""
+        else:
+            print("Guess again.")
+            attempts -= 1
+            print(f"You have {attempts} attempts remaining to guess the number.")
+            guessTheNumber()
+    """if attempts is equal to 1, the user runs out of guesses and he loses"""
+    if (attempts == 1):
+        print("You have run out of guesses, you lose!")
+"""initially the function is called"""
+guessTheNumber()
